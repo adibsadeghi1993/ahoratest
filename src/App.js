b3cd97a699c2efe3131e399d7f793025c792b559
adibsadeghi <adibsadeghi7272@gmail.com>
2021-05-24 16:28:55 +0430

    data fetched and products component was built

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+
+      useEffect(() => {
+        axios.get("http://localhost:3005/products")
+        .then(res=>{
+         setProducts(res.data)
+        }).catch(err=>console.log("err"))
+        
+      }, [])
+
   return (
     <div className="App">
-     
+      <Header />
+      <div className="container"></div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import "./header.css";
+import Badge from '@material-ui/core/Badge';
+
+const Header = () => {
+  return (
+    <div className="store">
+      <div>
+        <h2>store</h2>
+      </div>
+      <div className="icons">
+        <div className="homeIcon">
+        <HomeIcon color="primary" fontSize="large" />
+        </div>
+        
+
+        <div className="shoppingIcon">
+        <Badge badgeContent={4} color="secondary" >
+        <ShoppingCartIcon color="primary" fontSize="large" />
+        </Badge>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
